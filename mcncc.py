626785446a402dc9072f2adb9b83747f407e9754
Erik <51793348+ErikFaustmann@users.noreply.github.com>
2020-06-16 10:48:30 +0200

    Update mcncc.py

--- mcncc.py
+++ mcncc.py
@@ -1,14 +1,13 @@
 import os
 from os import path
 import matplotlib
-matplotlib.use('Agg')
 from PIL import Image
-
 import pickle
 import numpy as np
 from tempfile import TemporaryFile
 from tqdm import tqdm
 import argparse
+import logging
 
 import torch
 from torch.nn import functional as F
@@ -17,14 +16,20 @@ from torchvision.transforms import ToTensor
 from torch.autograd import Variable
 import torchvision
 from torchvision import transforms
-from torchvision.transforms import functional as F
+#from torchvision.transforms import functional as F
 import torchvision.models as models
 from torch import nn
 
 folder = path.expanduser('datasets/FID-300')
+
 parser = argparse.ArgumentParser(description='take some indiviudal folders from user')
-parser.add_argument('tracks', type = str, help='track folder')
-parser.add_argument('refs', type=str, help='refs folder')
+parser.add_argument('-t', '--tracks', type = str, default='tracks_cropped_Subset', help='define track folder')
+parser.add_argument('-rf', '--refs', type=str, default='Subset', help='define reference folder')
+parser.add_argument('-r', '--rot', default=False, action='store_true', help='add rotation')
+parser.add_argument('-sf', '--scorefile', type=str, default='scores.npy', help='scorefilename')
+parser.add_argument('-cmc', '--cmc', default=False, action='store_true', help='calculate cmc')
+parser.add_argument('-cmcf', '--cmc_file', type=str, default='cmc_file',help='cmc filename')
+
 args = parser.parse_args()
 
 tracks = path.join(folder, args.tracks)
@@ -34,8 +39,13 @@ ref_l = [f for f in os.listdir(refs) if f.endswith('.png')]
 track_l = [f for f in os.listdir(tracks) if f.endswith('.jpg')]
 
 if __name__ == "__main__":
-    print(refs)
-    print(tracks)
+    print("refs:", args.refs)
+    print("tracks:", args.tracks)
+    print("rot:", args.rot)
+    print("cmc:", args.cmc)
+    print("scorefile:", args.scorefile)
+    print("cmc_file:", args.cmc_file)
+    print(type(args.cmc_file))
 
 device = torch.device('cuda:0')
 
@@ -51,45 +61,66 @@ trans = transforms.Compose([
                                  std=[0.229, 0.224, 0.225])
 ])
 
-"""    
-Normalized Cross-Correlation for pattern matching.
-pytorch implementation
-roger.bermudez@epfl.ch
-CVLab EPFL 2019
-"""
-
-import logging
-import torch
-from torch.nn import functional as F
-
-
 ncc_logger = logging.getLogger(__name__)
 
+def compute_cmc(score_mat):
+    
+    test_labels = [26, 8, 28, 37, 23, 5, 17, 27, 1, 15, 15, 8, 8, 30, 35, 6, 1, 32, 22, 1, 1, 19, 13, 20, 1, 7, 21, 36, 3, 12, 33, 9, 34, 38, 12, 11, 10, 14, 16, 29, 4, 24, 4, 2, 33, 3, 18, 31, 25, 25]
+
+    true_mat = np.zeros((50, 38))
+
+    for i in range(len(test_labels)):
+        true_mat[i, test_labels[i]-1]= 1
+    
+    cmc = np.zeros(score_mat.shape[1], dtype='float64' )
+    mx = np.zeros(score_mat.shape[0], dtype='float64')
+    true_mat_est = np.zeros(score_mat.shape)
+    est_loc =np.zeros(score_mat.shape[0])
+    score_mat2 = score_mat
+
+    for i in range(score_mat.shape[1]):
+#________________________________________________________________________________ 
+    #mx = np.zeros(score_mat.shape[0], dtype='float64')
+
+        for w in range(score_mat.shape[0]):
+            mx[w] = max(score_mat2[w])
+    #print(mx)
+#________________________________________________________________________________ 
+    #true_mat_est = np.zeros(score_mat.shape)
+
+        for e in range(score_mat.shape[0]):
+            true_mat_est[e]  = np.equal(score_mat2[e], mx[e])
+
+            est_loc[e] = list(true_mat_est[e]).index(1)
+        if i == 0:
+            with np.printoptions(threshold=np.inf):
+                #print(true_mat_est)
+                print(est_loc)
+
+        true_mat_est = true_mat_est*1
+    #print(i, ":", sum(true_mat_est))
+#________________________________________________________________________________     
+    
+        if i == 0:
+            cmc[i] = np.tensordot(true_mat, true_mat_est, axes=2)/score_mat.shape[0]
+            #print("correct matches:", numpy.tensordot(true_mat, true_mat_est, axes=2))
+        else:
+            cmc[i] = (np.tensordot(true_mat, true_mat_est, axes=2)/score_mat.shape[0])+ cmc[i-1]
+            #print("correct matches:", numpy.tensordot(true_mat, true_mat_est, axes=2))
+#________________________________________________________________________________ 
+        for g in range(score_mat.shape[0]):
+            score_mat2[g][int(est_loc[g])] = -100000
+            #print(est_loc[g])
+            #print(score_mat2[g])
+
+    return cmc
 
 def patch_mean(images, patch_shape):
-    """
-    Computes the local mean of an image or set of images.
-    Args:
-        images (Tensor): Expected size is (n_images, n_channels, *image_size). 1d, 2d, and 3d images are accepted.
-        patch_shape (tuple): shape of the patch tensor (n_channels, *patch_size)
-    Returns:
-        Tensor same size as the image, with local means computed independently for each channel.
-    Example::
-        >>> images = torch.randn(4, 3, 15, 15)           # 4 images, 3 channels, 15x15 pixels each
-        >>> patch_shape = 3, 5, 5                        # 3 channels, 5x5 pixels neighborhood
-        >>> means = patch_mean(images, patch_shape)
-        >>> expected_mean = images[3, 2, :5, :5].mean()  # mean of the third image, channel 2, top left 5x5 patch
-        >>> computed_mean = means[3, 2, 5//2, 5//2]      # computed mean whose 5x5 neighborhood covers same patch
-        >>> computed_mean.isclose(expected_mean).item()
-        1
-    """
+
     channels, *patch_size = patch_shape
     dimensions = len(patch_size)
-    
-#   padding = (0,0)
     padding = tuple(side // 2 for side in patch_size)
-    #padding = tuple(min(10, side // 2) for side in patch_size)
-#     padding = tuple(0 for side in template_shape)
+
 
     conv = (F.conv1d, F.conv2d, F.conv3d)[dimensions - 1]
 
@@ -99,39 +130,20 @@ def patch_mean(images, patch_shape):
     weights = weights.to(images.device)
 
     # Make convolution operate on single channels
-    channel_selector = torch.eye(channels).byte()
-    weights[1 - channel_selector] = 0
+    channel_selector = torch.eye(channels).bool()
+    weights[~channel_selector] = 0
 
     result = conv(images, weights, padding=padding, bias=None)
     return result
 
 
 def patch_std(image, patch_shape):
-    """
-    Computes the local standard deviations of an image or set of images.
-    Args:
-        images (Tensor): Expected size is (n_images, n_channels, *image_size). 1d, 2d, and 3d images are accepted.
-        patch_shape (tuple): shape of the patch tensor (n_channels, *patch_size)
-    Returns:
-        Tensor same size as the image, with local standard deviations computed independently for each channel.
-    Example::
-        >>> images = torch.randn(4, 3, 15, 15)           # 4 images, 3 channels, 15x15 pixels each
-        >>> patch_shape = 3, 5, 5                        # 3 channels, 5x5 pixels neighborhood
-        >>> stds = patch_std(images, patch_shape)
-        >>> patch = images[3, 2, :5, :5]
-        >>> expected_std = patch.std(unbiased=False)     # standard deviation of the third image, channel 2, top left 5x5 patch
-        >>> computed_std = stds[3, 2, 5//2, 5//2]        # computed standard deviation whose 5x5 neighborhood covers same patch
-        >>> computed_std.isclose(expected_std).item()
-        1
-    """
     
     return (patch_mean(image**2, patch_shape) - patch_mean(image, patch_shape)**2).sqrt()
 
 
 def channel_normalize(template):
-    """
-    Z-normalize image channels independently.
-    """
+
     reshaped_template = template.clone().view(template.shape[0], -1)
     reshaped_template.sub_(reshaped_template.mean(dim=-1, keepdim=True))
     reshaped_template.div_(reshaped_template.std(dim=-1, keepdim=True, unbiased=False)+10e-10)
@@ -140,23 +152,7 @@ def channel_normalize(template):
 
 
 class NCC(torch.nn.Module):
-    """
-    Computes the [Zero-Normalized Cross-Correlation][1] between an image and a template.
-    Example:
-        >>> lena_path = "https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png"
-        >>> lena_tensor = torch.Tensor(plt.imread(lena_path)).permute(2, 0, 1).cuda()
-        >>> patch_center = 275, 275
-        >>> y1, y2 = patch_center[0] - 25, patch_center[0] + 25
-        >>> x1, x2 = patch_center[1] - 25, patch_center[1] + 25
-        >>> lena_patch = lena_tensor[:, y1:y2 + 1, x1:x2 + 1]
-        >>> ncc = NCC(lena_patch)
-        >>> ncc_response = ncc(lena_tensor[None, ...])
-        >>> ncc_response.max()
-        tensor(1.0000, device='cuda:0')
-        >>> np.unravel_index(ncc_response.argmax(), lena_tensor.shape)
-        (0, 275, 275)
-    [1]: https://en.wikipedia.org/wiki/Cross-correlation#Zero-normalized_cross-correlation_(ZNCC)
-    """
+
     def __init__(self, template, keep_channels=False):
         super().__init__()
 
@@ -164,21 +160,15 @@ class NCC(torch.nn.Module):
 
         channels, *template_shape = template.shape
         dimensions = len(template_shape)
-        
-        #self.padding = (0,0)
         self.padding = tuple(side // 2 for side in template_shape)
-        #print(self.padding)
-        #self.padding = tuple(min(10, side // 2) for side in template_shape)
-        #print("self.padding:", self.padding)
-#         self.padding = tuple(0 for side in template_shape)
 
         self.conv_f = (F.conv1d, F.conv2d, F.conv3d)[dimensions - 1]
         self.normalized_template = channel_normalize(template)
         ones = template.dim() * (1, )
         self.normalized_template = self.normalized_template.repeat(channels, *ones)
         # Make convolution operate on single channels
-        channel_selector = torch.eye(channels).byte()
-        self.normalized_template[1 - channel_selector] = 0
+        channel_selector = torch.eye(channels).bool()
+        self.normalized_template[~channel_selector] = 0
         # Reweight so that output is averaged
         patch_elements = torch.Tensor(template_shape).prod().item()
         self.normalized_template.div_(patch_elements)
@@ -200,19 +190,71 @@ class NCC(torch.nn.Module):
 
 score_mat = np.zeros((len(np.sort(track_l)), (len(np.sort(ref_l)))), dtype='float64')
 
-for x, t in enumerate(tqdm(np.sort(track_l))):
-    for y, r in enumerate(np.sort(ref_l)):
-        image = Image.open(path.join(refs, r))
+if args.rot == False:
+
+    for x, t in enumerate(tqdm(np.sort(track_l))):
+
         template = Image.open(path.join(tracks,t))
-        
         template_t = model(trans(template).unsqueeze(0).to(device))[0]
-        image_t = model(trans(image).unsqueeze(0).to(device))
-
-        template_t2 = template_t[:, 3:template_t.shape[1]-3, 3:template_t.shape[2]-3].to(device)
-        image_t2 = image_t[:,:,3:image_t.shape[2]-3, 3:image_t.shape[3]-3].to(device)
-
+        template_t2 = template_t[:, 3:template_t.shape[1]-3, 3:template_t.shape[2]-3]
         ncc = NCC(template_t2)
-        ncc_response = ncc(image_t2)
-        score_mat[x][y] = np.amax(ncc_response.cpu().data.numpy())
+        
+        for y, r in enumerate(np.sort(ref_l)):
+            image = Image.open(path.join(refs, r))
+            image_t = model(trans(image).unsqueeze(0).to(device))
+            image_t2 = image_t[:,:,3:image_t.shape[2]-3, 3:image_t.shape[3]-3].to(device)
+
+            ncc_response = ncc(image_t2)
+            score_mat[x][y] = np.amax(ncc_response.cpu().data.numpy())
+
+else:
     
-np.save('scores.npy', score_mat)
+    for x, t in enumerate(tqdm(np.sort(track_l))):
+        
+        template = Image.open(path.join(tracks,t))
+        template_t = model(trans(template).unsqueeze(0).to(device))[0]
+        template_t2 = template_t[:, 3:template_t.shape[1]-3, 3:template_t.shape[2]-3]
+        ncc = NCC(template_t2)
+
+        for y, r in enumerate(np.sort(ref_l)):
+            
+            img = Image.open(path.join(refs, r))
+            for i in range(-5, 6):
+                if i == -5:
+                    img2 = img.rotate(i, expand=1, fillcolor='white')
+                    img2 = trans(img2).unsqueeze(0)
+                    img_batch = torch.zeros(11, 3, img2.shape[2], img2.shape[3])
+                else:
+                    img2 = img.rotate(i)
+                    img2 = trans(img2).unsqueeze(0)
+                    img_batch[i][:, :img2[0].shape[1],:img2[0].shape[2]] = img2[0]
+
+            img_t_batch = model(img_batch.to(device))
+
+            ncc_response = ncc(img_t_batch) 
+            
+            
+            cc = 0
+
+            for i in range(10):
+                if cc < torch.max(ncc_response[i]).item():
+                    cc = torch.max(ncc_response[i]).item()
+                    
+            score_mat[x][y] = cc
+    
+np.save(args.scorefile, score_mat)
+
+if args.cmc == True:
+    
+    cmc_score = compute_cmc(score_mat)
+
+    f, ax = plt.subplots(1)
+    x_data = np.arange(-10, 11)
+    plt.plot(x_data, cmc_score.detach().numpy())
+    plt.gca().legend(('img_rotation','img_rotation'))
+    plt.suptitle('Track: 1 Reference: 1044')
+    plt.xlabel('Angle [deg]')
+    plt.ylabel('Correleation')
+    ax.set_ylim(bottom=0)
+    plt.grid(True)
+    f.savefig(args.cmc_file)
